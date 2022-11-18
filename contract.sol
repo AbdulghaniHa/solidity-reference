e477ea3cbfd0d3e52607cf13a8f6ef202488c526
AbdulghaniHarran <abdulghani@rafraf.com>
2022-11-18 15:51:01 +0300

    functions

--- contract.sol
+++ contract.sol
@@ -33,6 +33,7 @@ contract Contract {
             Reference: Array
     */
 
+    // Those are "state variables" because declared on level 1 of contract
     string public myString = "String";
     bool public myBoolean = true;
     uint public myUint = 1;
@@ -44,7 +45,34 @@ contract Contract {
     int public maxInt = type(int).max;
 
     /* 2) Functions
-        function [FUNCTION_NAME()]
+        function FUNCTION_NAME(DATATYPE VARIABLE_NAME) VISIBILITY READ/WRITE_TYPE returns(RETURNED_DATATYPE) {
+            LOGIC
+            return LOGIC
+        }
+
+        pure: read function + CAN NOT read data from blockchain and state variables or smart contracts
+        view: read function + CAN read data from blockchain and state variables or smart contracts
+
+    */
+
+    function add(uint x, uint y) external pure returns(uint) {
+        uint sum = x + y;   // Local variable 
+        return sum;
+    }
+
+    function globalVars() external view returns(address, uint, uint) {
+        // Global variables: stores data from blockchain
+        address sender = msg.sender;            // The address of the caller
+        uint timestamp = block.timestamp;       // Timestamp of the calling request of contract
+        uint blockNum = block.number;           // Stores the block number
+        return (sender, timestamp, blockNum);
+    }
+
+
+
+    /*
+
+
     */
 }   
 
